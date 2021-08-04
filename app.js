078a4105a77bb080cfd240ff21a40161ee897eee
Stephanie Di Giannantonio <stephaniedigiannantonio323@gmail.com>
2021-08-04 20:44:09 -0300

    direccion de entrega

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 let nombreUsuario = prompt('Por favor, ingrese su nombre :)')
 
 
+
 function compraRealizada(precioLibro){
-    let cantidadLibros = prompt('¿Cuantas unidades desea adquirir?')
+    let cantidadLibros = prompt('¿Cuantas unidades desea adquirir?' );
     let precioFinal = precioLibro * cantidadLibros;
     let valorIva = 1.21;
+    let direccionCliente = prompt('Por favor indicar su direccion');
 
 
-    alert(`${nombreUsuario}, el precio final es: ${precioFinal * valorIva}`);
+    alert(`${nombreUsuario}, el precio final es: ${precioFinal * valorIva} , \n El pedido se entregara en: ${direccionCliente}`);
+    
+    
  }
