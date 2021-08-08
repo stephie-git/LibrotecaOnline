9fcd77037e8195d0ded8d57780ce31e5c323e068
Stephanie Di Giannantonio <stephaniedigiannantonio323@gmail.com>
2021-08-08 20:35:42 -0300

    objeto

--- Images/LA TIA COSIMA LIBRO_files/jquery.noConflict.js(1).download
+++
@@ -1,2 +0,0 @@
-/*! jQuery noConflict */
-var $JM = jQuery.noConflict();
--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 let nombreUsuario = prompt('Por favor, ingrese su nombre :)')
 
+let valorIva = 1.21;
+
 
 
 function compraRealizada(precioLibro){
     let cantidadLibros = prompt('¿Cuantas unidades desea adquirir?' );
     let precioFinal = precioLibro * cantidadLibros;
-    let valorIva = 1.21;
     let direccionCliente = prompt('Por favor indicar su direccion');
+    let nuevaCompra = new Compra(nombreUsuario, precioFinal, cantidadLibros, direccionCliente);
 
 
-    alert(`${nombreUsuario}, el precio final es: ${precioFinal * valorIva} , \n El pedido se entregara en: ${direccionCliente}`);
-    
-    
+    alert(`Hola ${nuevaCompra.cliente}, sus ${nuevaCompra.productos} libro/s seran despachados a ${nuevaCompra.direccion}, y el valor es de ${nuevaCompra.precioFinal}. \n El precio con IVA es: ${nuevaCompra.calculoIva()}`);
+    console.log(nuevaCompra)
+    }
+
+ class Compra{
+    constructor(cliente, precioFinal, cantidadProductos, direccionEnvio){
+        this.cliente = cliente;
+        this.precioFinal = precioFinal;
+        this.productos = cantidadProductos;
+        this.direccion = direccionEnvio;
+     }
+     calculoIva(){
+        let iva = this.precioFinal = this.precioFinal * this.productos * valorIva; 
+        return iva
+     }
  }
+
+
+
+
+
