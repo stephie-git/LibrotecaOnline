1f947fabbdf29c0afffac54d04931e261c9bd572
Stephanie Di Giannantonio <stephaniedigiannantonio323@gmail.com>
2021-08-16 23:01:27 -0300

    objetos libros

--- app.js
+++ app.js
@@ -1,9 +1,54 @@
-let arrayLibros= []; 
-let arrayCompras= [];
-let nombreUsuario = prompt('Por favor, ingrese su nombre :)')
+let prueba = document.getElementById('divPrueba');
+let parrafo = document.createElement('p');
+parrafo.innerHTML = 'Compra con un solo click y recibilos desde tu casa!!'
+prueba.appendChild(parrafo);
+prueba.classList.add('estilosDiv');
 
 let valorIva = 1.21;
 
+let arrayCompras= [];
+arrayLibros=[
+   {
+      titulo:'Después', 
+      parrafo:'En ocasiones; para crecer debes vencer a tus demonios. El rey del terror regresa con una nueva e inolvidable novela. N° 1 en la lista de más vendidos del New York Times. (...)',
+      autor:'Stephen King',
+      imagen:'images/Stephen King.jpg',
+      editorial: 'Puerto de palos',
+      genero:'terror',
+      precio: '700'
+   },
+   {
+      titulo: 'La tia Cosima',
+      parrafo:'Cósima; un nombre para recordar. pepiUna historia de cómo el amor vence al odio. La nueva novela de Florencia Bonelli; la escritora argentina más leída y admirada de América Latina; vuelve con una historia contemporánea donde coexisten la profesión; la amistad; el amor y un pasado doloroso que; (...)',
+      autor: 'Florencia Bonelli',
+      imagen: "images/la tia cosi.jpg",
+      editorial: 'Suma',
+      genero: 'Romance',
+      precio: '600'
+
+   },
+   {
+      titulo: 'El hogar de las niñas indeseadas',
+      parrafo: 'Un estudio de cómo el amor persiste incluso en los momentos más difíciles. Una novela profunda y significativa; que captura la atención del lector hasta el final. Booklist Philomena y El tren de los huérfanos confluyen en esta desgarradora historia sobre el lazo irrompible entre una madre y s (...)',
+      autor: 'Joanna Goodman',
+      imagen: "images/El hogar de las niñas.jpg",
+      editorial: 'ABC',
+      genero: 'Drama',
+      precio: '570'
+   },
+
+   {
+      titulo: 'Asesino de brujas',
+      parrafo: 'La esperada secuela del éxito de ventas del New York Times: Asesino de brujas. La bruja blanca. Los riesgos son mayores. brujas son más letales. Y el romance es aún más intenso. (...)',
+      autor: 'Shelby Mahurin',
+      imagen: "images/asesino de brujas.jpg",
+      editorial: 'Blend',
+      genero: 'Suspenso',
+      precio: '900'
+   },
+];
+
+let nombreUsuario = prompt('Por favor, ingrese su nombre :)')
 
 
 function compraRealizada(precioLibro){
@@ -34,12 +79,13 @@ function compraRealizada(precioLibro){
  }
 
  class Libro{
-    constructor(titulo, autor, editorial, genero)
+    constructor(titulo, autor, editorial, genero, imagen)
     {
        this.titulo = titulo;
        this.autor = autor;
        this.editorial = editorial;
        this.genero = genero;
+       this.imagen = imagen;
     }
  }
 
@@ -50,24 +96,38 @@ function compraRealizada(precioLibro){
    let editorialLibro = prompt('Indique la editorial del libro')
    let generoLibro = prompt('Indique el titulo del libro')
 
-   let libroCreado = new Libro(tituloLibro, autorLibro, editorialLibro, generoLibro);
+   let libroCreado = new Libro(tituloLibro, autorLibro, editorialLibro, generoLibro, imagen);
    arrayLibros.push(libroCreado)
 }
 
- let nuevoLibro1 = new Libro('Despues', 'Stephen King', 'Santillana', 'Terror');
- let nuevoLibro2 = new Libro('La tia cosima', 'Florencia Bonelli', 'Suma', 'drama');
- let nuevoLibro3 = new Libro('El hogar de las niñas indeseadas', 'Joanna Goodman', 'Las rosas', 'drama');
- let nuevoLibro4 = new Libro('Asesino de brujas', 'Shelby Mahurin', 'Los hijos de rey', 'suspenso');
-
- 
- arrayLibros.push(nuevoLibro1, nuevoLibro2, nuevoLibro3, nuevoLibro4);
  arrayLibros.sort();
  
- console.log(arrayLibros);
+ function crearTarjetas(libros){
+    let contenedorProductos = document.getElementById('productos');
+
+    libros.forEach(element =>{
+      console.log(element)
+      let divTarjeta = document.createElement('div');
+      let tapaLibros = document.createElement('img');
+      let tituloLibros = document.createElement('h4');
+      let botonCompra = document.createElement('button');
+      let parrafoLibros = document.createElement('p');
+      divTarjeta.classList.add('card');
+
+      tapaLibros.src = element.imagen;
+      tituloLibros.innerHTML = element.titulo;
+      parrafoLibros.innerHTML = element.parrafo;
+      botonCompra.innerHTML = 'Comprar'
+     
+
+      divTarjeta.append(tapaLibros, tituloLibros, parrafoLibros, botonCompra);
+      contenedorProductos.appendChild(divTarjeta);
+
+      botonCompra.addEventListener('click', event=>compraRealizada(element.precio))
+   })
+}
 
 
 
-
-
-
+crearTarjetas(arrayLibros)
 
